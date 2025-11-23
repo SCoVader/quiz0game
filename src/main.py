b1b86cb3a5f320a83a02e05522c4205314dc522e
Jnx Jnx <SCoVader@proton.me>
2025-11-23 19:41:54 +0200

    Trying to figure out data structures and flow

---
+++ src/main.py
@@ -0,0 +1,88 @@
+import os
+import sqlite3
+
+def demo():
+    import logging
+    import threading
+    from quiz0_server import Quiz0Server
+    from quiz0_handler import Quiz0RequestHandler
+    logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s')
+
+    address = ('localhost', 0) # let the kernel give us a port
+    server = Quiz0Server(address, Quiz0RequestHandler)
+    ip, port = server.server_address # find out what port we were given
+
+    t = threading.Thread(target=server.serve_forever)
+    # t.setDaemon(True) # don't hang on exit
+    t.daemon = True # don't hang on exit
+    t.start()
+
+    logger = logging.getLogger('client')
+    logger.info('Server on %s:%s', ip, port)
+
+    # Connect to the server
+    logger.debug('creating socket')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    logger.debug('connecting to server')
+    s.connect((ip, port))
+
+    # Send the data
+    message = 'Hello, world'
+    logger.debug('sending data: "%s"', message)
+    # len_sent = s.send(message)
+    len_sent = s.send(bytes(message, 'utf-8'))
+
+    # Receive a response
+    logger.debug('waiting for response')
+    response = s.recv(len_sent)
+    logger.debug('response from server: "%s"', response)
+
+    # Clean up
+    logger.debug('closing socket')
+    s.close()
+    logger.debug('done')
+    server.socket.close()
+
+
+if __name__ == '__main__':
+
+    db_path = "tutorial.db"
+    if os.path.exists(db_path):
+        os.remove(db_path)
+
+
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute("create table movie(title, year, score)")
+    cursor.execute("""
+INSERT INTO movie VALUES
+    ('Monty Python and the Holy Grail', 1975, 8.2),
+    ('And Now for Something Completely Different', 1971, 7.5)
+""")
+    connection.commit()
+
+    data = [
+        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+        ("Monty Python's The Meaning of Life", 1983, 7.5),
+        ("Monty Python's Life of Brian", 1979, 8.0)
+    ]
+    cursor.executemany("insert into movie values(?, ?, ?)", data)
+    connection.commit()
+
+    for row in cursor.execute("SELECT year, title FROM movie ORDER BY year"):
+        print(row)
+
+    connection.close()
+
+    new_con = sqlite3.connect("tutorial.db")
+    new_cur = new_con.cursor()
+    res = new_cur.execute("SELECT title, year FROM movie ORDER BY score DESC")
+    title, year = res.fetchone()
+    print(f'The highest scoring Monty Python movie is {title!r}, released in {year}')
+    new_con.close()
+
+    # Select quiz by number
+    # get question
+    # give ansver
+    # next
+    pass
--- src/structures.py
+++ src/structures.py
@@ -2,16 +2,46 @@ class Player():
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        
-class Quiz():
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def set_next(self, node):
+        self.next = node
+
+    def __repr__(self):
+        return self.val
+
+class Quiz:
+    def add_to_head(self, node):
+        node.next = self.head
+        self.head = node
+
+    def add_to_tail(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        last_node = None
+        for current_node in self:
+            last_node = current_node
+        last_node.set_next(node)
+
     def __init__(self):
         self.head = None
 
-    def add_question(self, question):
-        if not quiestion: return
-        if not self.head: 
-            self.head = question
-            return 
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)
         
 
 class Question():
